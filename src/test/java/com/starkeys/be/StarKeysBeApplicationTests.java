0c81acb7bb482458f20c665b8dc2f86eb5b93822
rojojun <zoloman316@gmail.com>
2025-10-05 01:39:10 +0900

    test: 테스트코드 설정 수정

--- src/test/java/com/starkeys/be/StarKeysBeApplicationTests.java
+++ src/test/java/com/starkeys/be/StarKeysBeApplicationTests.java
@@ -1,10 +1,10 @@
 package com.starkeys.be;
 
+import com.starkeys.be.configuration.IntegrationTestSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class StarKeysBeApplicationTests {
+class StarKeysBeApplicationTests extends IntegrationTestSupport {
 
     @Test
     void contextLoads() {
